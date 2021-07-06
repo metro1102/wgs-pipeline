581564f86dada056c64d4058f87d62c2ab2ec6ef
George Testo <metro1102@gmail.com>
2021-07-06 13:44:58 -0700

    Update script comments
    Standardizes comments and adds block headers for critical sections.

--- wgs-pipeline.sh
+++ wgs-pipeline.sh
@@ -6,9 +6,9 @@
 #SBATCH --time=72:00:00
 #SBATCH --mem=80G
 
-###########
-## SETUP ##
-###########
+###############################################################################
+#                                    Setup                                    #
+###############################################################################
 
 # Please configure the following settings:
 ROOT="/scratch/gtesto/projects/" # Enter the root directory for your projects.
@@ -20,11 +20,13 @@ PIPELINE="kraken2" # Enter your desired pipeline ("kraken2" or "metaphlan").
 # If using kraken2, please specify the database path to use.
 DATABASE="/labs/Microbiome/gtesto/databases/silva_138.1_SSU"
 
-## !DO NOT EDIT BEYOND THIS POINT! ##
 
-####################
-## INITIALIZATION ##
-####################
+###################### ! DO NOT EDIT BEYOND THIS POINT ! ######################
+
+
+###############################################################################
+#                                Initalization                                #
+###############################################################################
 
 # Initate bash shell using conda
 source ~/.bashrc
@@ -34,12 +36,11 @@ cd ${ROOT}${PROJECT_NAME}
 
 mkdir ${SAMPLE_TYPE} && cd ${SAMPLE_TYPE}
 
-################
-## CONDITIONS ##
-################
+###############################################################################
+#                             Specific Conditions                             #
+###############################################################################
 
-# FRAGMENT_TYPE CONDITIONS
-
+# FRAGMENT_TYPE
 if [ FRAGMENT_TYPE="paired" ] || [ FRAGMENT_TYPE="single"]; then
 
   echo "Running sequence read(s) in ${FRAGMENT_TYPE} end mode..."
@@ -52,9 +53,7 @@ elif [ -z "$FRAGMENT_TYPE" ]; then
 
 fi
 
-# PIPELINE CONDITIONS
-
-
+# PIPELINE
 if [ PIPELINE="kraken2" ] || [ PIPELINE="metaphlan" ]; then
 
   echo "Running the ${PIPELINE} workflow..."
@@ -77,9 +76,9 @@ elif [ -z "$PIPELINE" ]; then
 
 fi
 
-###################
-## RUN KNEADDATA ##
-###################
+###############################################################################
+#                                Run Kneaddata                                #
+###############################################################################
 
 # Activate kneaddata conda environment
 conda activate kneaddata-0.7.4
@@ -134,7 +133,7 @@ kneaddata_read_count_table \
   --input results \
   --output reports/kneaddata/reads_report.kneaddata
 
-# Deactivate kneaddata conda environment
+# Deactivate kneadddata conda environment
 conda deactivate
 
 # Clean up file structure
@@ -164,11 +163,11 @@ mv *log logs/.
 
 cd ..
 
-#####################
-## QUALITY CONTROL ##
-#####################
+###############################################################################
+#                               Quality Control                               #
+###############################################################################
 
-# Activate the QC conda environment
+# Activate QC conda environment
 conda activate kneaddata-0.7.4
 
 # Create output folders
@@ -179,248 +178,248 @@ mkdir reports/multiqc/after_trimming
 fastqc -t 8 results/paired/*.fastq --outdir=reports/fastqc/after_trimming
 multiqc reports/fastqc/after_trimming --outdir=reports/multiqc/after_trimming
 
-# Deactivate the QC conda environment
+# Deactivate QC conda environment
 conda deactivate
+
+###############################################################################
+#                              Pipeline Script(s)                             #
+###############################################################################
 
 if [ PIPELINE="kraken2" ]; then
 
-  #################
-  ## RUN KRAKEN2 ##
-  #################
+ ###############################################################################
+ #                                 Run kraken2                                 #
+ ###############################################################################
 
-  # Activate kraken2 conda environment
-  conda activate kraken2-2.1.2
+ # Activate kraken2 conda environment
+ conda activate kraken2-2.1.2
 
-  # Create output folders
-  mkdir reports/kraken2
-  mkdir results/kraken2
+ # Create output folders
+ mkdir reports/kraken2
+ mkdir results/kraken2
 
-  # Run kraken2 on available trims
+ # Run kraken2 on available reads
+ if [ FRAGMENT_TYPE="paired" ]; then
 
-  if [ FRAGMENT_TYPE="paired" ]; then
+ for i in results/paired/*_paired_1.fastq;
+ do
+   filename=$(basename "$i");
+   fname="${filename%_paired_*.fastq}";
+   kraken2 --db ${DATABASE} --threads 8 --use-names --output results/kraken2/${fname}_output.kraken --report reports/kraken2/${fname}_report.kraken --paired results/paired/${fname}_paired_1.fastq results/paired/${fname}_paired_2.fastq
+ done
 
-    for i in results/paired/*_paired_1.fastq;
-    do
-      filename=$(basename "$i");
-      fname="${filename%_paired_*.fastq}";
-      kraken2 --db ${DATABASE} --threads 8 --use-names --output results/kraken2/${fname}_output.kraken --report reports/kraken2/${fname}_report.kraken --paired results/paired/${fname}_paired_1.fastq results/paired/${fname}_paired_2.fastq
-    done
+ elif [ FRAGMENT_TYPE="single" ]; then
 
-  elif [ FRAGMENT_TYPE="single" ]; then
+ for i in results/single/*.fastq
+ do
+   filename=$(basename "$i");
+   fname="${filename%*.fastq}";
+   kraken2 --db ${DATABASE} --threads 8 --use-names --output results/kraken2/${fname}_output.kraken --report reports/kraken2/${fname}_report.kraken results/single/${fname}.fastq
+ done
 
-    for i in results/single/*.fastq
-    do
-      filename=$(basename "$i");
-      fname="${filename%*.fastq}";
-      kraken2 --db ${DATABASE} --threads 8 --use-names --output results/kraken2/${fname}_output.kraken --report reports/kraken2/${fname}_report.kraken results/single/${fname}.fastq
-    done
+ fi
 
-  fi
+ # Deactivate kraken2 conda environment
+ conda deactivate
 
-  # Deactivate kraken2 conda environment
-  conda deactivate
+ #############################################################################
+ #                                Run bracken                                #
+ #############################################################################
 
-  #################
-  ## RUN BRACKEN ##
-  #################
+ conda activate kraken2-2.1.2
 
-  # Activate kraken2 conda environment
-  conda activate kraken2-2.1.2
+ mkdir results/kraken2/bracken
+ mkdir reports/kraken2/bracken
 
-  # Create output folder
-  mkdir results/kraken2/bracken
-  mkdir reports/kraken2/bracken
+ cd reports/kraken2
 
-  cd reports/kraken2
+ for i in *_report.kraken;
+ do
+   filename=$(basename "$i");
+   fname="${filename%_report.kraken}";
+   bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l S -i $i -o bracken/${fname}_species_output.bracken -w bracken/${fname}_species_report.bracken;
+ done
 
-  # Run Bracken on available kraken2 reports
-  for i in *_report.kraken;
-  do
-    filename=$(basename "$i");
-    fname="${filename%_report.kraken}";
-    bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l S -i $i -o bracken/${fname}_species_output.bracken -w bracken/${fname}_species_report.bracken;
-  done
+ for i in *_report.kraken;
+ do
+   filename=$(basename "$i");
+   fname="${filename%_report.kraken}";
+   bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l G -i $i -o bracken/${fname}_genus_output.bracken -w bracken/${fname}_genus_report.bracken;
+ done
 
-  for i in *_report.kraken;
-  do
-    filename=$(basename "$i");
-    fname="${filename%_report.kraken}";
-    bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l G -i $i -o bracken/${fname}_genus_output.bracken -w bracken/${fname}_genus_report.bracken;
-  done
+ for i in *_report.kraken;
+ do
+   filename=$(basename "$i");
+   fname="${filename%_report.kraken}";
+   bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l P -i $i -o bracken/${fname}_phylum_output.bracken -w bracken/${fname}_phylum_report.bracken;
+ done
 
-  for i in *_report.kraken;
-  do
-    filename=$(basename "$i");
-    fname="${filename%_report.kraken}";
-    bracken -d /labs/Microbiome/gtesto/databases/all_nucleotide -l P -i $i -o bracken/${fname}_phylum_output.bracken -w bracken/${fname}_phylum_report.bracken;
-  done
+ # This step is necessary, and we should combine outputs from kraken2 & bracken
+ # Because reads shouldn't be combined before processing, this step is important
+ combine_bracken_outputs.py --files bracken/*species.bracken -o bracken/all_species_output.bracken
+ combine_bracken_outputs.py --files bracken/*genus.bracken -o bracken/all_genus_output.bracken
+ combine_bracken_outputs.py --files bracken/*phylum.bracken -o bracken/all_phylum_output.bracken
 
-  # This step is necessary, and we should combine outputs from kraken2 & bracken
-  # Because reads shouldn't be combined before processing, this step is important
-  combine_bracken_outputs.py --files bracken/*species.bracken -o bracken/all_species_output.bracken
-  combine_bracken_outputs.py --files bracken/*genus.bracken -o bracken/all_genus_output.bracken
-  combine_bracken_outputs.py --files bracken/*phylum.bracken -o bracken/all_phylum_output.bracken
+ mv *_output.bracken ../../../results/kraken2/bracken
+ mv *all* ../../../results/kraken2/bracken
 
-  mv *_output.bracken ../../../results/kraken2/bracken
-  mv *all* ../../../results/kraken2/bracken
+ # Deactivate kraken2 conda environment
+ conda deactivate
 
-  # Deactivate kraken2 conda environment
-  conda deactivate
+ cd ../../../
 
-  cd ../../../
+ ###############################################################################
+ #                                  Run krona                                  #
+ ###############################################################################
 
-  ###############
-  ## RUN KRONA ##
-  ###############
+ # Activate krona conda environment
+ conda activate krona-2.8
 
-  # Activate krona conda environment
-  conda activate krona-2.8
+ # Create output folders
+ mkdir reports/krona
+ mkdir reports/krona/kraken2
+ mkdir reports/krona/bracken
 
-  # Create output folders
-  mkdir reports/krona
-  mkdir reports/krona/kraken2
-  mkdir reports/krona/bracken
+ cd reports/kraken2
 
-  cd reports/kraken2
+ # Generate krona using species reports from bracken
+ for i in *_report.kraken;
+ do
+   filename=$(basename "$i");
+   fname="${filename%_report.kraken}";
+   python /labs/Microbiome/gtesto/scripts/KrakenTools-1.2/kreport2krona.py -r ${filename} -o ../krona/kraken2/${fname}.krona;
+ done
 
-  # Generate krona using species reports from bracken
-  for i in *_report.kraken;
-  do
-    filename=$(basename "$i");
-    fname="${filename%_report.kraken}";
-    python /labs/Microbiome/gtesto/scripts/KrakenTools-1.2/kreport2krona.py -r ${filename} -o ../krona/kraken2/${fname}.krona;
-  done
+ ktImportText ../krona/kraken2/*.krona -o ../krona/kraken2.krona.html
 
-  ktImportText ../krona/kraken2/*.krona -o ../krona/kraken2.krona.html
+ cd bracken
+ for i in *_species_report.bracken;
+ do
+   filename=$(basename "$i")
+   fname="${filename%_species_report.bracken}";
+   python /labs/Microbiome/gtesto/scripts/KrakenTools-1.2/kreport2krona.py -r ${filename} -o ../../krona/bracken/${fname}.krona;
+ done
 
-  cd bracken
-  for i in *_species_report.bracken;
-  do
-    filename=$(basename "$i")
-    fname="${filename%_species_report.bracken}";
-    python /labs/Microbiome/gtesto/scripts/KrakenTools-1.2/kreport2krona.py -r ${filename} -o ../../krona/bracken/${fname}.krona;
-  done
+ ktImportText ../../krona/bracken/*.krona -o ../../krona/bracken.krona.html
 
-  ktImportText ../../krona/bracken/*.krona -o ../../krona/bracken.krona.html
+ # Deactivate krona conda environment
+ conda deactivate
 
-  # Deactivate krona conda environment
-  conda deactivate
+ cd ../../../
 
-  cd ../../../
+ ###############################################################################
+ #                                   Run biom                                  #
+ ###############################################################################
 
-  ##############
-  ## RUN BIOM ##
-  ##############
+ # Activate biom conda environment
+ conda activate kraken-biom-1.0.1
 
-  # Activate biom conda environment
-  conda activate kraken-biom-1.0.1
+ cd reports/kraken2/bracken
 
-  cd reports/kraken2/bracken
+ # Temporarily copy & rename bracken species reports
+ mkdir temp
 
-  # Temporarily copy & rename bracken species reports
-  mkdir temp
+ for i in *_species_report.bracken
+ do
+   filename=$(basename "$i")
+   fname="${filename%_species_report.bracken}";
+   cp ${filename} temp/${fname}.bracken
+ done
 
-  for i in *_species_report.bracken
-  do
-    filename=$(basename "$i")
-    fname="${filename%_species_report.bracken}";
-    cp ${filename} temp/${fname}.bracken
-  done
+ cd temp
 
-  cd temp
+ # Generate biom format & summarize the results
+ kraken-biom *.bracken -o sequences.biom --fmt json
+ biom summarize-table -i sequences.biom -o sequences-summary.txt
 
-  # Generate biom format & summarize the results
-  kraken-biom *.bracken -o sequences.biom --fmt json
-  biom summarize-table -i sequences.biom -o sequences-summary.txt
+ # Deactivate biom conda environment
+ conda deactivate
 
-  # Deactivate biom conda environment
-  conda deactivate
+ # Move biom files back to the main directory
+ mv sequences.biom ../../../../
+ mv sequences-summary.txt ../../../../
 
-  # Move biom files back to the main directory
-  mv sequences.biom ../../../../
-  mv sequences-summary.txt ../../../../
+ # Remove temporary folder
+ cd .. && rm -r temp
 
-  # Remove temporary folder
-  cd .. && rm -r temp
+ cd ../../../
 
-  cd ../../../
-
-  # Rename biom files to sample type
-  #mkdir results/biom
-  mv sequences.biom ${SAMPLE_TYPE}-bracken-results.biom
-  mv sequences-summary.txt ${SAMPLE_TYPE}-bracken-summary.txt
+ # Rename biom files to sample type
+ #mkdir results/biom
+ mv sequences.biom ${SAMPLE_TYPE}-bracken-results.biom
+ mv sequences-summary.txt ${SAMPLE_TYPE}-bracken-summary.txt
 
 elif [ PIPELINE="metaphlan" ]; then
 
-  ####################
-  ## RUN METAPHLAN2 ##
-  ####################
+ ###############################################################################
+ #                                Run metaphlan                                #
+ ###############################################################################
 
-  # Activate metaphlan conda environment
-  conda activate metaphlan-3.0.10
+ # Activate metaphlan conda environment
+ conda activate metaphlan-3.0.10
 
-  # Create output folders
-  mkdir results/bowtie2
-  mkdir reports/metaphlan
+ # Create output folders
+ mkdir results/bowtie2
+ mkdir reports/metaphlan
 
-  # Run metaphlan on available trims
-  for i in results/paired/*_paired_1.fastq
-  do
-    filename=$(basename "$i")
-    fname="${filename%_paired_*.fastq}";
-    metaphlan --bowtie2db /labs/Microbiome/gtesto/databases/bowtie2 results/paired/${fname}_paired_1.fastq,results/paired/${fname}_paired_2.fastq --input_type fastq --bowtie2out results/bowtie2/${fname}.bowtie2.bz2 -o reports/metaphlan/${fname}.txt
-  done
+ # Run metaphlan on available trims
+ for i in results/paired/*_paired_1.fastq
+ do
+   filename=$(basename "$i")
+   fname="${filename%_paired_*.fastq}";
+   metaphlan --bowtie2db /labs/Microbiome/gtesto/databases/bowtie2 results/paired/${fname}_paired_1.fastq,results/paired/${fname}_paired_2.fastq --input_type fastq --bowtie2out results/bowtie2/${fname}.bowtie2.bz2 -o reports/metaphlan/${fname}.txt
+ done
 
-  cd reports/metaphlan
+ cd reports/metaphlan
 
-  merge_metaphlan_tables.py * > merged_abundance_table.txt
+ merge_metaphlan_tables.py * > merged_abundance_table.txt
 
-  grep -E "s__|clade" merged_abundance_table.txt | sed 's/^.*s__//g'\
-    | cut -f1,3-8 | sed -e 's/clade_name/SampleID/g' > merged_abundance_table_species.txt
+ grep -E "s__|clade" merged_abundance_table.txt | sed 's/^.*s__//g'\
+   | cut -f1,3-8 | sed -e 's/clade_name/SampleID/g' > merged_abundance_table_species.txt
 
-  # Move and rename merged_abundance_table_species
-  mv merged_abundance_table_species.txt ../../${SAMPLE_TYPE}-metaphlan-results.txt
+ # Move & rename merged_abundance_table_species
+ mv merged_abundance_table_species.txt ../../${SAMPLE_TYPE}-metaphlan-results.txt
 
-  # Deactivate metaphlan conda environment
-  conda deactivate
+ # Deactivate metaphlan conda environment
+ conda deactivate
 
-  cd ../../
+ cd ../../
 
-  ###############
-  ## RUN KRONA ##
-  ###############
+ ###############################################################################
+ #                                  Run krona                                  #
+ ###############################################################################
 
-  # Activate krona conda environment
-  conda activate krona-2.8
+ # Activate krona conda environment
+ conda activate krona-2.8
 
-  # Create output folders
-  mkdir reports/krona
-  mkdir reports/krona/metaphlan
+ # Create output folders
+ mkdir reports/krona
+ mkdir reports/krona/metaphlan
 
-  cd reports/metaphlan
+ cd reports/metaphlan
 
-  # Generate krona using species reports from bracken
-  python /labs/Microbiome/gtesto/scripts/metaphlan2krona.py -r ../metaphlan/merged_abundance_table.txt -o ../krona/metaphlan/merged_abundance_table.krona
+ # Generate krona using species reports from bracken
+ python /labs/Microbiome/gtesto/scripts/metaphlan2krona.py -r ../metaphlan/merged_abundance_table.txt -o ../krona/metaphlan/merged_abundance_table.krona
 
-  ktImportText ../krona/metaphlan/*.krona -o ../krona/metaphlan.krona.html
+ ktImportText ../krona/metaphlan/*.krona -o ../krona/metaphlan.krona.html
 
-  # Deactivate krona conda environment
-  conda deactivate
+ # Deactivate krona conda environment
+ conda deactivate
 
-  cd ../../
+ cd ../../
 
-  #################
-  ## RUN HCLUST2 ##
-  #################
+ ###############################################################################
+ #                                 Run hclust2                                 #
+ ###############################################################################
 
-  # Activate hclust2 conda environment
-  conda activate hclust2
+ # Activate hclust2 conda environment
+ conda activate hclust2
 
-  # Run hclust2 on metaphlan results
-  hclust2.py -i ${SAMPLE_TYPE}-metaphlan-results.txt -o reports/metaphlan/abundance_heatmap_species.png --ftop 25 --f_dist_f braycurtis --s_dist_f braycurtis --cell_aspect_ratio 0.5 -l --flabel_size 6 --slabel_size 6 --max_flabel_len 100 --max_slabel_len 100 --minv 0.1 --dpi 300
+ # Run hclust2 on metaphlan results
+ hclust2.py -i ${SAMPLE_TYPE}-metaphlan-results.txt -o reports/metaphlan/abundance_heatmap_species.png --ftop 25 --f_dist_f braycurtis --s_dist_f braycurtis --cell_aspect_ratio 0.5 -l --flabel_size 6 --slabel_size 6 --max_flabel_len 100 --max_slabel_len 100 --minv 0.1 --dpi 300
 
-  # Deactivate hclust2 conda environment
-  conda deactivate
+ # Deactivate hclust2 conda environment
+ conda deactivate
 
 fi
 
