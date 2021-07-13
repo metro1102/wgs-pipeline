c1089cad4c715fb7104f4f799b636301cdcba088
George Testo <metro1102@gmail.com>
2021-07-13 11:16:41 -0700

    Remove ANSI colors
    Removes ANSI color escape coding for init, info, and error messages; this will clean up log messages for users.

--- functions.sh
+++ functions.sh
@@ -6,13 +6,13 @@
 
 # Logging #####################################################################
 initLog() { # Init Log (for initalization messages)
-    echo -e "\e[37m""INIT - $1""\e[0m"
+    echo -e "INIT - $1"
 }
 
 infoLog() { # Info Log (for task specific messages)
-    echo -e "\e[36m""INFO - $1""\e[0m"
+    echo -e "INFO - $1"
 }
 
 errorLog() { # Error log (for error messages)
-    echo -e "\e[91m""ERROR - $1""\e[0m"
+    echo -e "ERROR - $1"
 }
